ac617ea265c886f4a18020cfc692ed39bdb6bf54
Nisanth <nisanth12.2005@gmail.com>
2026-01-23 22:50:29 +0530

    Create full adder.v

---
+++ Gate Level Modelling/full adder.v
@@ -0,0 +1,11 @@
+module full_adder(input a,b,c,
+                  output sum,carry,
+                  wire w1,w2,w3
+                 );
+  xor(sum,a,b,c);
+  and(w1,a,c);
+  and(w2,b,c);
+  and(w3,a,b);
+  or(carry,w1,w2,w3);
+endmodule
+  
